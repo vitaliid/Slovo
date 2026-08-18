4c8f480c7536c6eb688ce27c9ddbedeeb088fbc2
vitalik_nba <vitalik.dudok@gmail.com>
2026-08-19 02:49:40 +0300

    Added project draft

---
+++ Unit1.dfm
@@ -0,0 +1,26 @@
+﻿object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Slovo'
+  ClientHeight = 441
+  ClientWidth = 624
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  Menu = MainMenu1
+  TextHeight = 15
+  object MainMenu1: TMainMenu
+    Left = 32
+    Top = 32
+    object miFile: TMenuItem
+      Caption = '&Файл'
+      object miExit: TMenuItem
+        Caption = '&Вихід'
+        OnClick = miExitClick
+      end
+    end
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,33 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus;
+
+type
+  TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    miFile: TMenuItem;
+    miExit: TMenuItem;
+    procedure miExitClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.miExitClick(Sender: TObject);
+begin
+  Close;
+end;
+
+end.
